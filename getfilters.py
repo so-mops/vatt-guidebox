6fd20dc3d90308244875af59fd42d3a347f7fa09
scott <scott@email.arizona.edu>
2019-10-28 16:47:48 -0700

    added comments to the getfilters.py script

--- getfilters.py
+++ getfilters.py
@@ -1,14 +1,23 @@
-
 import socket
 import re
 
 
+
+
 class vatt_filters:
+    """Class to retrieve filter data from the
+    redesigned guidebox at VATT."""
 
     def __init__(self):
         pass
 
     def connect(self, ip:str="10.0.1.108", port:int=7623):
+        """
+        Description: Use socket class to create a connection
+        to the INDI server that controls the guidebox
+        Args ip, port
+        Return: socket connection instance
+        """
         soc = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
         HOST = socket.gethostbyname(ip)
         soc.settimeout(0.1)
@@ -17,6 +26,18 @@ class vatt_filters:
 
 
     def getfilters(self):
+        """
+        Description: Connects to the INDI server and sends
+        a getProperties xml tag. Then listen for a response.
+        the response should come in the form of an xml tag
+        like:
+        <message device="FILTERS" message="upper:clear lower:U">
+        A regular expresion is then used to extract the filters
+        in the beam in the upper and lower wheels.
+
+        Returns: A dictionary with the filters in the beam.
+
+        """
         conn = self.connect()
         qrystring = b"<getProperties version='1.7' device='FILTERS' />"
         conn.send(qrystring)
